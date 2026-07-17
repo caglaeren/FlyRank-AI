c527344e2a0950a9dba478e30d9b4da4425a95b8
Tuğba Çağla EREN <tugbacaglaeren@gmail.com>
2026-07-17 23:46:46 +0300

    base 3: POST /tasks endpoint added

--- Backend AI Engineering/Week 2/my_crud_api/main.py
+++ Backend AI Engineering/Week 2/my_crud_api/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class TaskCreate(BaseModel):
+    title : str
 
 
 #id, title, done olacak
@@ -38,4 +42,25 @@ def read_task(task_id : int):
         if task["id"] == task_id:
             return task
     raise HTTPException(status_code=404, detail=f"Task {task_id} not found.")
-     
+
+
+#post yani yeni task ekleyelim
+#201: created yani oluşturuldu isteğin başarıyla işlendiği anlamına gelir
+@app.post("/tasks", status_code=201)
+def create_task(task: TaskCreate):
+    #title boş ise error ver
+    if not task.title or not task.title.strip():
+        raise HTTPException(status_code=400, detail="Task title is required.")
+
+    # yeni id oluşturalım
+    new_id = max([t["id"] for t in tasks]) + 1
+
+    new_task = {
+        "id" : new_id,
+        "title" : task.title,
+        "done" : False
+    }
+    tasks.append(new_task)
+    return new_task
+
+
