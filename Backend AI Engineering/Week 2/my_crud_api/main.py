98825cee1273d3aa6776ba8c6b2b35c5526a7d5a
Tuğba Çağla EREN <tugbacaglaeren@gmail.com>
2026-07-18 00:47:21 +0300

    Stage 4: Completed full CRUD operations (PUT & DELETE)

--- Backend AI Engineering/Week 2/my_crud_api/main.py
+++ Backend AI Engineering/Week 2/my_crud_api/main.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from typing import Optional
+from fastapi import status
 
 app = FastAPI()
 
 class TaskCreate(BaseModel):
     title : str
 
+class TaskUpdate(BaseModel):
+    title : Optional[str] = None
+    done: Optional[bool] = None
 
 #id, title, done olacak
 tasks = [
@@ -64,3 +69,45 @@ def create_task(task: TaskCreate):
     return new_task
 
 
+#put : güncelleme yapalım, idye göre
+#Unknown id → 404
+#Empty/invalid body -> 400
+@app.put("/tasks/{task_id}", status_code=status.HTTP_200_OK)
+def update_task(task_id: int, task_update: TaskUpdate):
+    # 1- taskı bulalım
+    task = None
+    for t in tasks:
+        if t["id"] == task_id:
+            task = t
+            break
+    
+    if task is None:
+        raise HTTPException(status_code=404, detail=f"Task {task_id} not found.")
+
+    # 2- doğrulama yapalım
+    if task_update.title is None and task_update.done is None:
+        raise HTTPException(status_code=400, detail="At least one field must be provided. ('title' / 'done')")
+    
+    # 3- güncelleme - update
+    if task_update.title is not None:
+        if not task_update.title.strip():
+            raise HTTPException(status_code=400, detail="Title can not be empty.")
+        task["title"] = task_update.title.strip()
+    if task_update.done is not None:
+        task["done"] = task_update.done
+
+    return task
+
+
+
+# delete : silme yapalım
+# 204 -> no content (içerik yok) 
+#204: sunucunun bir istemci isteğini başarıyla yerine getirdiğini ancak yanıt olarak herhangi bir veri veya içerik göndermediğini belirten
+
+@app.delete("/tasks/{task_id}", status_code=204)
+def delete_task(task_id:int):
+    for i, task in enumerate(tasks):
+        if task["id"] == task_id:
+            tasks.pop(i)
+            return
+    raise HTTPException(status_code=404, detail=f"Task {task_id} not found.")
